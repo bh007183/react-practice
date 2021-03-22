c598e61e09471c9480cd9317ec7798fe97f7b785
Ben Hopkins <bjhops17@gmail.com>
2021-03-22 07:38:37 -0700

    creation of context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {Context} from "./pages/context"
+import React, {useState, useMemo} from "react";
+import Body from "./components/body"
+import Sibling from "./components/sibling"
+
 
 function App() {
+
+  const [color, setColor] = useState ({
+    color: ""
+  })
+
+  const memo = useMemo(() => ({color, setColor}), [color, setColor])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Context.Provider value={memo}>
+        <Body/>
+        <Sibling/>
+
+      </Context.Provider>
+      
     </div>
   );
 }
---
+++ src/components/sibling.js
@@ -0,0 +1,13 @@
+const { individualProduct, setIndividualProduct } = useContext(Context);
+import React, {  useContext } from "react";
+import { Context } from "../contexts";
+
+import React from 'react'
+
+export default function Sibling() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
